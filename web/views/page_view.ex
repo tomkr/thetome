769860555ede12e2506813a3bbc888909565628e
Tom Kruijsen <tom@tomkr.org>
2015-11-05 13:35:52 +0100

    Little styling on detail page.

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,7 +1,7 @@
 defmodule Thetome.PageView do
   use Thetome.Web, :view
 
-  def cover_image_url book do
-    "http://covers.openlibrary.org/b/isbn/#{book.isbn}-S.jpg"
+  def cover_image_url book, size \\ "S" do
+    "http://covers.openlibrary.org/b/isbn/#{book.isbn}-#{size}.jpg"
   end
 end
